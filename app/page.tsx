95dc7fca88d89780fafa95e33ec082ddc04fc9f3
Pustak Pathak <74170759+PustakP@users.noreply.github.com>
2024-03-16 16:08:32 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import './globals.css';
+import styles from '../styles/snow.module.scss';
+
+
+const snowFlakes = Array.from(Array(200).keys());
+
+export default function Page() {
+  return (
+    <div className=''>
+       
+        <div className='flex justify-center items-center h-screen w-screen'>
+          <h1 className='text-[900px]'>Sex</h1>
+        </div>
+
+    </div>
+      
+
+  );
+}
