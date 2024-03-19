3471657ca808310b08ecfe2308d96b049ca3cc57
Pustak Pathak <74170759+PustakP@users.noreply.github.com>
2024-03-19 13:57:36 +0530

    terimaakiaak

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+
 export default function Page() {
   return (
     <div className='overflow-hidden'>
       <div className='flex justify-center items-center h-screen w-screen'>
-        <h1 className='text-[50px] sm:text-[310px]'>i wanted to drink more</h1>
+        {/* <h1 className='text-[50px] sm:text-[310px]'>i wanted to drink more</h1> */}
+
+        <Card className="w-[72%] h-[80%]">
+          <CardHeader className="py-10 justify-center items-center">
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent className="px-20 py-8">
+            <p>Card Content</p>
+          </CardContent>
+        </Card>
+
       </div>
     </div>
   );
